3f7d9c45c075f7a1597ea3663b5b5c411bcb0742
Tom Burgan <tmb4x@hscmail.mcc.virginia.edu>
2019-03-05 15:33:33 -0500

    Changed column pointers

--- Python/CHCAHPS/ParsePatientExperienceGoalsCSV.py
+++ Python/CHCAHPS/ParsePatientExperienceGoalsCSV.py
@@ -5,7 +5,8 @@ from tabula import read_pdf
 from pandas import read_csv
 
 dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Goals\\Provided by Patient Experience\\110518"
-dnameout = "F:\\Visual Studio 2017\\Projects\\Python\\CHCAHPS"
+# dnameout = "F:\\Visual Studio 2017\\Projects\\Python\\CHCAHPS"
+dnameout = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Goals\\Cleaned"
 
 fnamein = "CHCAHPS PX Goal Setting As Of Jun (SL FINAL 20181012 V13).csv"
 fnameout = "CHCAHPS PX Goal Setting As Of Jun (SL FINAL 20181012 V13).txt"
@@ -46,17 +47,19 @@ for i,row in df.iterrows():
 		if index == 0: Service_Line = topbox_li[index]
 		if index == 1: Unit_Location = topbox_li[index]
 		if index == 2: Domain_Question = topbox_li[index]
-		if index == 20: FY2017_Score = topbox_li[index]
-		if index == 28: FY2018_Score = topbox_li[index]
+		# if index == 20: FY2017_Score = topbox_li[index]
+		if index == 24: FY2017_Score = topbox_li[index]
+		# if index == 28: FY2018_Score = topbox_li[index]
+		if index == 35: FY2018_Score = topbox_li[index]
 		if index == 47: FY2019_Score = topbox_li[index]
 	
 	dfo = dfo.append(dict(zip(columns, (Service_Line, Unit_Location, Domain_Question, FY2017_Score, FY2018_Score, FY2019_Score))),ignore_index=True)
 
-##print(dfo)
+# print(dfo)
 
 dfo = dfo.fillna('')
 dfo.rename(index=str, columns={"FY2017_Score": "2017", "FY2018_Score": "2018", "FY2019_Score": "2019"}, inplace=True)
-#dfo.head()
+# dfo.head()
 
 dfo_unpivot = pd.melt(dfo.replace('null',np.nan),
    id_vars=['Service_Line','Unit_Location','Domain_Question'],
@@ -65,17 +68,12 @@ dfo_unpivot = pd.melt(dfo.replace('null',np.nan),
    .dropna() \
    .sort_values(['Service_Line','Unit_Location','Domain_Question'])
 
-#array = df.values
-#array = dfo.values
 array = dfo_unpivot.values
-##print(array)
+# print(array)
 df = None
 dfo = None
-##np.savetxt(fo, array, fmt='%s', delimiter=",")
-##np.savetxt("temp.txt", array, fmt='%s', delimiter=",")
-#np.savetxt(fnameout, array, fmt='%s', delimiter=",")
 np.savetxt(fo, array, fmt='%s', delimiter=",")
 fo.close
 dfo_unpivot = None
-#df = None
+df = None
 array = None
