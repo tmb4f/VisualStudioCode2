c133b9519f385657e28fd964d16bf28757666da1
Tom Burgan <tmb4x@hscmail.mcc.virginia.edu>
2019-09-06 18:16:19 -0400

    Edit extract script, change skiprows property

--- Python/CHCAHPS/ParsePatientExperienceFY20GoalsCSV.py
+++ Python/CHCAHPS/ParsePatientExperienceFY20GoalsCSV.py
@@ -159,7 +159,7 @@ array = None
 
 Service = 'HCAHPS'
 
-df = read_csv(dnamein_HCAHPS + "\\" + fnamein_HCAHPS, usecols = [0, 1, 2, 3, 4, 45], skiprows=7, dtype=str)
+df = read_csv(dnamein_HCAHPS + "\\" + fnamein_HCAHPS, usecols = [0, 1, 2, 3, 4, 45], skiprows=6, dtype=str)
 
 #print(df)
 
