f45805c6570e6b9456f63850918558199508bbf3
Tom Burgan <tmb4f@hscmail.mcc.virginia.edu>
2020-03-20 09:06:53 -0400

    Edit code for March 2020

--- PythonApp/CHCAHPS/ParsePressGaneyRanksPDF.py
+++ PythonApp/CHCAHPS/ParsePressGaneyRanksPDF.py
@@ -26,7 +26,8 @@ from tabula import read_pdf
 # dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\111819"
 # dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\121819"
 # dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\011720"
-dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\021420"
+# dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\021420"
+dnamein = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Original From Client\\031920"
 dnameout = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient Experience\\CHCAHPS\\Data\\Percentile Rank\\Cleaned"
 
 #fnamein = "2018_07 CHCAHPS TB_R - All PG Database_CMS View.pdf"
@@ -67,8 +68,10 @@ dnameout = "O:\\Computing Services\\INFSUP_S\\Documentation\\Projects\\Patient E
 # fnameout = "2019_12 CHCAHPS TB_R - All PG Database_CMS View.txt"
 # fnamein = "2020_01 CHCAHPS TB_R - All PG Database_CMS View.pdf"
 # fnameout = "2020_01 CHCAHPS TB_R - All PG Database_CMS View.txt"
-fnamein = "2020_02 CHCAHPS TB_R - All PG Database_CMS View.pdf"
-fnameout = "2020_02 CHCAHPS TB_R - All PG Database_CMS View.txt"
+# fnamein = "2020_02 CHCAHPS TB_R - All PG Database_CMS View.pdf"
+# fnameout = "2020_02 CHCAHPS TB_R - All PG Database_CMS View.txt"
+fnamein = "2020_03 CHCAHPS TB_R - All PG Database_CMS View.pdf"
+fnameout = "2020_03 CHCAHPS TB_R - All PG Database_CMS View.txt"
 
 fo = open(dnameout + "\\" + fnameout,'wb')
 
