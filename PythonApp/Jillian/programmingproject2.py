ea5f761d6610f91776c363d806e9eff7fde57e9b
Tom Burgan <tmb4f@hscmail.mcc.virginia.edu>
2022-07-15 13:02:00 -0400

    Edit programmingproject2

--- PythonApp/Jillian/programmingproject2.py
+++ PythonApp/Jillian/programmingproject2.py
@@ -3,6 +3,10 @@ student_names = ['Jennifer','John','Margaret','Jack','Jillian','Adam','Ali','Chr
 out_of_state = [True,False,False,True,False,False,True,True,True,False]
 table1 = pd.DataFrame(list(zip(student_names, out_of_state)), columns=["Student","Status"])
 print(table1)
+#
+# Use 
+for i,row in table1.iterrows(): # Converts each dataframe row to a series named "row"
+    table1_li = row.tolist() # Transform series to a list object
+    if table1_li[1] == True: print(table1_li[0]) # Use list indexing to select list values
 for i,row in table1.iterrows():
-    table1_li = row.tolist() # Transform table1 data row into a list
-    if table1_li[1] == True: print(table1_li[0])
+    if row.Status == True: print(row.Student) # Use series indexing to select series values
